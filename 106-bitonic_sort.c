22fd30d0a16a8f1f6781f9e24a79984b23f05596
jhay-smart <omalejoy83@gmail.com>
2023-08-28 16:09:45 -0700

    commited

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -4,6 +4,11 @@
 void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
 void bitonic_merge(int *array, int l, int r, int direction);
 
+/**
+ * bitonic_sort - sorts an array following the Bitonic sort algorithm
+ * @array: array of ints to sort
+ * @size: size of the array
+ */
 void bitonic_sort(int *array, size_t size)
 {
 	if (!array || size < 2)
@@ -12,6 +17,14 @@ void bitonic_sort(int *array, size_t size)
 	bitonic_recursion(array, 0, size - 1, 1, size);
 }
 
+/**
+ * bitonic_recursion - recursive function for bitonic sort
+ * @array: array to sort
+ * @l: index of the left-most element
+ * @r: index of the right-most element
+ * @direction: ascending or descending
+ * @size: size of the array
+ */
 void bitonic_recursion(int *array, int l, int r, int direction, size_t size)
 {
 	int step;
@@ -37,6 +50,13 @@ void bitonic_recursion(int *array, int l, int r, int direction, size_t size)
 	}
 }
 
+/**
+ * bitonic_merge - sorts and merges a sequence in ascending or descending order
+ * @array: array to sort
+ * @l: index of the left-most element
+ * @r: index of the right-most element
+ * @direction: ascending or descending
+ */
 void bitonic_merge(int *array, int l, int r, int direction)
 {
 	int tmp, i, step = (l + r) / 2, mid = (r - l + 1) / 2;
